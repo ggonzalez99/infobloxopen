c631128bdb9354c2381443e7c00a5420210b7e71
George Gonzalez <107451052+ggonzalez99@users.noreply.github.com>
2022-06-16 20:37:53 -0700

    Finally! v2
    Had to restart. My file triggered antivirus somehow.
    
    Language: Go
    GitUI: Github Desktop
    IDE: Goland
    
    (started with CLI until errors started to happen such as outdated directories)

---
+++ GoTestRepo/helloInfoblox.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Infoblox!")
+}
+
+/*
+My steps:
+run in terminal
+   go run helloInfoblox.go
+
+got results
+everytime a major update is done to the code
+Github Desktop syncs itself and will provide easy
+option to push and commit to Github
+I hope
+*/
